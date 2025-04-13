0f09d60f9276ad6e2616a8cdfcdff5fde35642b1
mjosephan2 <mario.kosasih@gmail.com>
2025-04-13 21:42:21 +0800

    add max area

---
+++ coding-go/water_container/maxarea_test.go
@@ -0,0 +1,43 @@
+package watercontainer
+
+import "testing"
+
+func Test_maxArea(t *testing.T) {
+	type args struct {
+		height []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "normal case",
+			args: args{
+				height: []int{1, 8, 6, 2, 5, 4, 8, 3, 7},
+			},
+			want: 49,
+		},
+		{
+			name: "single",
+			args: args{
+				height: []int{1, 1},
+			},
+			want: 1,
+		},
+		{
+			name: "zero height",
+			args: args{
+				height: []int{0, 0},
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxArea(tt.args.height); got != tt.want {
+				t.Errorf("maxArea() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
