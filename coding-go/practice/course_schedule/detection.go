d0aee2d8e500256c66d0c94c7a1c12c13eab9510
DESKTOP-589F6VG\mario <mario.kosasih@gmail.com>
2025-08-04 21:18:04 +0800

    detection

---
+++ coding-go/practice/course_schedule/detection.go
@@ -0,0 +1,50 @@
+package main
+
+/*
+	1. build graph
+	2. for loop each node to detect cycle
+	3. if cycle detected, return false
+*/
+func canFinish(numCourses int, prerequisites [][]int) bool {
+	graph := make([][]int, numCourses)
+	for i := 0; i < numCourses; i++ {
+		graph[i] = make([]int, 0)
+	}
+
+	for _, pre := range prerequisites {
+		x := pre[0]
+		y := pre[1]
+		graph[x] = append(graph[x], y)
+	}
+	// graph[x] -> get x neighbors, need those neighbors to be done first
+	// it becomes impossible if there
+
+	visited := make([]int, numCourses)
+	for i := 0; i < numCourses; i++ {
+		if hasCycle(graph, visited, i) {
+			return false // Cycle detected
+		}
+	}
+
+	return true
+}
+
+func hasCycle(graph [][]int, visited []int, node int) bool {
+	if visited[node] == 1 {
+		return true // Cycle detected
+	}
+	if visited[node] == -1 {
+		return false // Already visited and no cycle
+	}
+
+	visited[node] = 1 // Mark node as visited
+
+	for _, neighbor := range graph[node] {
+		if hasCycle(graph, visited, neighbor) {
+			return true // Cycle detected
+		}
+	}
+
+	visited[node] = -1 // Mark node as visited and no cycle
+	return false
+}
